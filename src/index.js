7b35755a1881bdf804dad78bbf63e1d43507bc17
Karen Alarcon <akareny.10@gmail.com>
2022-04-21 21:43:45 -0500

    modificacion espacios codigo

--- src/index.js
+++ src/index.js
@@ -14,11 +14,8 @@ const ncAnswer = validator.maskify(numeroTarjeta);
       "su tarjeta de credito terminada en " +
       ncAnswer +
       " fue validada satisfactoriamente!.";
-<<<<<<< HEAD
       document.getElementById("tarjetaDeCredito").value = "";
     
-=======
->>>>>>> 159baf0f1f2ba7e86e8f2658b551937d695f2172
     
 
     } else if (notification == false && ncAnswer != "") {
@@ -27,12 +24,8 @@ const ncAnswer = validator.maskify(numeroTarjeta);
       "su tarjeta de credito " +
       ncAnswer +
       " es inválida, por favor verifique su tarjeta!.";
-<<<<<<< HEAD
       document.getElementById("tarjetaDeCredito").value = "";
   }
-=======
-    }
->>>>>>> 159baf0f1f2ba7e86e8f2658b551937d695f2172
   else {
     alert("Ingrese un número de tarjeta");
   }
